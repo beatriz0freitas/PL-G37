55df3a77c31be5bcabe4a204edf41c07b26ee6db
luis444coelho <a106843@alunos.uminho.pt>
2026-04-28 14:27:09 +0100

    lixo

--- src/symbols.py
+++
@@ -1,5 +0,0 @@
-"""Compatibilidade para imports antigos da tabela de simbolos."""
-
-from src.analise_semantica.symbols import Symbol, SymbolTable, VALID_KINDS
-
-__all__ = ["Symbol", "SymbolTable", "VALID_KINDS"]
